170f4452fa18348d16891e02dd9ec98a1cb0bfd5
Alvin Waweru <81688085+Alvin1284@users.noreply.github.com>
2025-05-22 23:08:15 +0300

    Add database connection decorator for SQLite

---
+++ python-decorators-0x01/1-with_db_connection.py
@@ -0,0 +1,14 @@
+import sqlite3
+import functools
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect("users.db")
+        try:
+            result = func(conn, *args, **kwargs)
+            return result
+        finally:
+            conn.close()
+            print("Database connection closed.")
+    return wrapper
