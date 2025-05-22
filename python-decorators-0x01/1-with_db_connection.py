7cf3e216cccb5fefef632160b88a944d72cea9f9
Alvin Waweru <81688085+Alvin1284@users.noreply.github.com>
2025-05-22 23:12:47 +0300

    Add user retrieval function with automatic database connection handling

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -12,3 +12,13 @@ def with_db_connection(func):
             conn.close()
             print("Database connection closed.")
     return wrapper
+
+@with_db_connection 
+def get_user_by_id(conn, user_id): 
+    cursor = conn.cursor() 
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,)) 
+    return cursor.fetchone() 
+#### Fetch user by ID with automatic connection handling 
+
+user = get_user_by_id(user_id=1)
+print(user)
