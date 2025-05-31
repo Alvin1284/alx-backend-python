24d61d6a605a9348aaab3a47c2024845a215d18a
Alvin Waweru <81688085+Alvin1284@users.noreply.github.com>
2025-06-01 02:05:50 +0300

    Fix formatting in TestMemoize class for consistency

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -104,8 +104,8 @@ class TestMemoize(unittest.TestCase):
                 return self.a_method()
 
         with patch.object(
-            TestClass, 
-            "a_method", 
+            TestClass,
+            "a_method",
             return_value=42
         )as mock_method:
             obj = TestClass()
