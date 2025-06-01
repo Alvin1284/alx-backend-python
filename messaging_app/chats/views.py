fcf3f8e0d827e7d138f4c807c04a477c636a4616
Alvin Waweru <81688085+Alvin1284@users.noreply.github.com>
2025-06-01 20:45:17 +0300

    Add URL routing for Conversation and Message viewsets; implement filtering for messages and conversations

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -2,6 +2,7 @@
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django_filters import rest_framework as filters
 from .models import Conversation, Message
 from .serializers import (
     ConversationSerializer,
@@ -11,9 +12,43 @@ from .serializers import (
 )
 
 
+class MessageFilter(filters.FilterSet):
+    conversation = filters.UUIDFilter(field_name="conversation__conversation_id")
+    sender = filters.UUIDFilter(field_name="sender__user_id")
+    read = filters.BooleanFilter(field_name="read")
+    after_date = filters.DateTimeFilter(field_name="sent_at", lookup_expr="gte")
+    before_date = filters.DateTimeFilter(field_name="sent_at", lookup_expr="lte")
+
+    class Meta:
+        model = Message
+        fields = ["conversation", "sender", "read", "after_date", "before_date"]
+
+
+class ConversationFilter(filters.FilterSet):
+    participant = filters.UUIDFilter(field_name="participants__user_id")
+    after_date = filters.DateTimeFilter(field_name="created_at", lookup_expr="gte")
+    before_date = filters.DateTimeFilter(field_name="created_at", lookup_expr="lte")
+    has_unread = filters.BooleanFilter(method="filter_has_unread")
+
+    class Meta:
+        model = Conversation
+        fields = ["participant", "after_date", "before_date", "has_unread"]
+
+    def filter_has_unread(self, queryset, name, value):
+        if value:
+            return (
+                queryset.filter(messages__read=False)
+                .exclude(messages__sender=self.request.user)
+                .distinct()
+            )
+        return queryset
+
+
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ConversationFilter
 
     def get_serializer_class(self):
         if self.action == "create":
@@ -22,7 +57,8 @@ class ConversationViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         # Only show conversations the user is part of
-        return self.queryset.filter(participants=self.request.user)
+        queryset = self.queryset.filter(participants=self.request.user)
+        return queryset.prefetch_related("participants", "messages")
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -61,6 +97,8 @@ class ConversationViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = MessageFilter
 
     def get_serializer_class(self):
         if self.action == "create":
@@ -69,9 +107,10 @@ class MessageViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         # Only show messages in conversations the user is part of
-        return self.queryset.filter(
+        queryset = self.queryset.filter(
             conversation__participants=self.request.user
         ).order_by("-sent_at")
+        return queryset.select_related("sender", "conversation")
 
     def perform_create(self, serializer):
         # Automatically set the sender to the current user
